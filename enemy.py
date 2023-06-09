5d8fc76f7c7f59fac7e2844fd5e1baf633a5189b
Siztas <130640143+Siztas@users.noreply.github.com>
2023-06-09 19:36:30 +0800

    Add files via upload

--- enemy.py
+++ enemy.py
@@ -1,146 +1,155 @@
-from entity import Entity
+import pygame
 from settings import *
+from entity import Entity
 from support import *
 
-
 class Enemy(Entity):
-    def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player):
-        super().__init__(groups)
-        self.sprite_type = 'enemy'
+	def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player,trigger_death_particles,add_exp):
 
-        self.import_graphics(monster_name)
-        self.status = 'idle'
-        self.image = self.animations[self.status][self.frame_index]
+		#在Entity里面建立敌人类
+		super().__init__(groups)
+		self.sprite_type = 'enemy'
 
-        # 行动
-        self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(0, -10)
-        self.obstacle_sprites = obstacle_sprites
+		#导入图片
+		self.import_graphics(monster_name)			#确定是哪一个类型的怪
+		self.status = 'idle'
+		self.image = self.animations[self.status][self.frame_index]
 
-        # stats
-        self.monster_name = monster_name
-        monster_info = monster_data[self.monster_name]
-        self.health = monster_info['health']
-        self.exp = monster_info['exp']
-        self.speed = monster_info['speed']
-        self.attack_damage = monster_info['damage']
-        self.resistance = monster_info['resistance']
-        self.attack_radius = monster_info['attack_radius']
-        self.notice_radius = monster_info['notice_radius']
-        self.attack_type = monster_info['attack_type']
+		# 怪物的移动
+		self.rect = self.image.get_rect(topleft = pos) 			#获取位置
+		self.hitbox = self.rect.inflate(0,-10) 					#设置碰撞的具体方块
+		self.obstacle_sprites = obstacle_sprites			    #障碍物（为了实现怪物与障碍物的碰撞）
 
-        # 互动
-        self.can_attack = True
-        self.attack_time = None
-        self.attack_cooldown = 400
-        self.damage_player = damage_player
+		# 怪物属性的设置
+		self.monster_name = monster_name
+		monster_info = monster_data[self.monster_name]				#根据名称从怪物集合中获取该怪物数据
+		self.health = monster_info['health']
+		self.exp = monster_info['exp']
+		self.speed = monster_info['speed']
+		self.attack_damage = monster_info['damage']				#伤害
+		self.resistance = monster_info['resistance']			#被攻击时被击退格
+		self.attack_radius = monster_info['attack_radius']		#攻击范围
+		self.notice_radius = monster_info['notice_radius']		#侦查半径
+		self.attack_type = monster_info['attack_type']			#攻击方式
 
-        self.vulnerable = True
-        self.hit_time = None
-        self.invincibility_duration = 300
+		# 与人物的互动
+		self.can_attack = True
+		self.attack_time = None
+		self.attack_cooldown = 400
+		self.damage_player = damage_player							#对人物的攻击（具体函数在level中）
+		self.trigger_death_particles = trigger_death_particles  	#死亡特效
+		self.add_exp = add_exp
 
-    def import_graphics(self, name):
-        self.animations = {'idle': [], 'move': [], 'attack': []}
-        main_path = f'../期末/敌人/{name}/'
-        for animation in self.animations.keys():
-            self.animations[animation] = import_folder(main_path + animation)
+		# 创建无敌帧（避免被连续快速多次攻击）
+		self.vulnerable = True
+		self.hit_time = None
+		self.invincibility_duration = 300
 
-    def get_player_distance_direction(self, player):
-        enemy_vec = pygame.math.Vector2(self.rect.center)
-        player_vec = pygame.math.Vector2(player.rect.center)
-        distance = (player_vec - enemy_vec).magnitude()
+		# 音效（可不加）
+		self.death_sound = pygame.mixer.Sound('../ONION/audio/death.wav')
+		self.hit_sound = pygame.mixer.Sound('../ONION/audio/hit.wav')
+		self.attack_sound = pygame.mixer.Sound(monster_info['attack_sound'])
+		self.death_sound.set_volume(0.6)
+		self.hit_sound.set_volume(0.6)
+		self.attack_sound.set_volume(0.6)
 
-        if distance > 0:
-            direction = (player_vec - enemy_vec).normalize()
-        else:
-            direction = pygame.math.Vector2()
+	def import_graphics(self,name):				#从文件夹中导入图片（因为存在多个相似文件夹，直接写了个函数解决）
+		self.animations = {'idle':[],'move':[],'attack':[]}
+		main_path = f'../ONION/graphics/monsters/{name}/'
+		for animation in self.animations.keys():
+			self.animations[animation] = import_folder(main_path + animation)
 
-        return (distance, direction)
+	def get_player_distance_direction(self,player):      		#获取与人物的方向与距离（用于移动）
+		enemy_vec = pygame.math.Vector2(self.rect.center)		#怪位置
+		player_vec = pygame.math.Vector2(player.rect.center)	#人物位置
+		distance = (player_vec - enemy_vec).magnitude()
 
-    def get_status(self, player):
-        distance = self.get_player_distance_direction(player)[0]
+		if distance > 0:
+			direction = (player_vec - enemy_vec).normalize()
+		else:
+			direction = pygame.math.Vector2()
 
-        if distance <= self.attack_radius and self.can_attack:
-            if self.status != 'attack':
-                self.frame_index = 0
-            self.status = 'attack'
-        elif distance <= self.notice_radius:
-            self.status = 'move'
-        else:
-            self.status = 'idle'
+		return (distance,direction)
 
-    def actions(self, player):
-        if self.status == 'attack':
-            self.attack_time = pygame.time.get_ticks()
-            self.damage_player(self.attack_damage, self.attack_type)
-        elif self.status == 'move':
-            self.direction = self.get_player_distance_direction(player)[1]
-        else:
-            self.direction = pygame.math.Vector2()
+	def get_status(self, player):          				#判断怪物下一步应该做什么
+		distance = self.get_player_distance_direction(player)[0]
 
-    def animate(self):
-        animation = self.animations[self.status]
-        self.frame_index += self.animation_speed
-        if self.frame_index >= len(animation):
-            if self.status == 'attack':
-                self.can_attack = False
-            self.frame_index = 0
+		if distance <= self.attack_radius and self.can_attack:
+			if self.status != 'attack':
+				self.frame_index = 0
+			self.status = 'attack'
+		elif distance <= self.notice_radius:
+			self.status = 'move'
+		else:
+			self.status = 'idle'
 
-        self.image = animation[int(self.frame_index)]
-        self.rect = self.image.get_rect(center=self.hitbox.center)
+	def actions(self,player):  			#进行怪物动作的实现
+		if self.status == 'attack':
+			self.attack_time = pygame.time.get_ticks()   #获取时间戳（为了实现攻击的冷却）
+			self.damage_player(self.attack_damage,self.attack_type)
+			self.attack_sound.play()
+		elif self.status == 'move':
+			self.direction = self.get_player_distance_direction(player)[1]
+		else:
+			self.direction = pygame.math.Vector2()
 
-        if not self.vulnerable:
-            alpha = self.wave_value()
-            self.image.set_alpha(alpha)
-        else:
-            self.image.set_alpha(255)
+	def animate(self):             #多张图片的反复播放
+		animation = self.animations[self.status]
+		
+		self.frame_index += self.animation_speed
+		if self.frame_index >= len(animation):
+			if self.status == 'attack':
+				self.can_attack = False
+			self.frame_index = 0
 
-    def cooldown(self):
-        current_time = pygame.time.get_ticks()
-        if not self.can_attack:
-            if current_time - self.attack_time >= self.attack_cooldown:
-                self.can_attack = True
+		self.image = animation[int(self.frame_index)]
+		self.rect = self.image.get_rect(center = self.hitbox.center)
 
-        if not self.vulnerable:
-            if current_time - self.hit_time >= self.invincibility_duration:
-                self.vulnerable = True
+		if not self.vulnerable:
+			alpha = self.wave_value()
+			self.image.set_alpha(alpha)
+		else:
+			self.image.set_alpha(255)
 
-    def get_damage(self, player, attack_type):
-        if self.vulnerable:
-            self.direction = self.get_player_distance_direction(player)[1]
-            if attack_type == 'weapon':
-                self.health -= player.get_full_weapon_damage()
-            else:
-                pass
-                # magic damage
-            self.hit_time = pygame.time.get_ticks()
-            self.vulnerable = False
+	def cooldowns(self):        # 制造攻击的冷却
+		current_time = pygame.time.get_ticks()
+		if not self.can_attack:
+			if current_time - self.attack_time >= self.attack_cooldown:
+				self.can_attack = True
 
-    def check_death(self):
-        if self.health <= 0:
-            self.kill()
+		if not self.vulnerable:
+			if current_time - self.hit_time >= self.invincibility_duration:
+				self.vulnerable = True
 
-    def hit_reaction(self):
-        if not self.vulnerable:
-            self.direction *= -self.resistance
+	def get_damage(self,player,attack_type):        #有关受到攻击的实现
+		if self.vulnerable:
+			self.hit_sound.play()
+			self.direction = self.get_player_distance_direction(player)[1]
+			if attack_type == 'weapon':
+				self.health -= player.get_full_weapon_damage()
+			else:
+				self.health -= player.get_full_magic_damage()      #判断是技能伤害还是武器伤害
+			self.hit_time = pygame.time.get_ticks()
+			self.vulnerable = False
 
-    def update(self):
-        self.hit_reaction()
-        self.move(self.speed)
-        self.animate()
-        self.cooldown()
-        self.check_death()
+	def check_death(self):      #确认是否死亡
+		if self.health <= 0:
+			self.kill()
+			self.trigger_death_particles(self.rect.center,self.monster_name)   #死亡特效
+			self.add_exp(self.exp)
+			self.death_sound.play()
 
-    def enemy_update(self, player):
-        self.get_status(player)
-        self.actions(player)
+	def hit_reaction(self):    #实现击退
+		if not self.vulnerable:
+			self.direction *= -self.resistance
 
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
+	def update(self):    #不断更新状态
+		self.hit_reaction()
+		self.move(self.speed)
+		self.animate()
+		self.cooldowns()
+		self.check_death()
 
-        self.hitbox.x += self.direction.x * speed * 12
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed * 12
-        self.collision('vertical')
-        self.rect.center = self.hitbox.center
+	def enemy_update(self,player):
+		self.get_status(player)
+		self.actions(player)
--- entity.py
+++ entity.py
@@ -1,44 +1,47 @@
 import pygame
 from math import sin
 
-
+#由于人物和怪物的移动与碰撞一模一样，所以直接创建一个父类
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, groups):
-        super().__init__(groups)
-        self.frame_index = 0
-        self.animation_speed = 0.15
-        self.direction = pygame.math.Vector2()
+	def __init__(self,groups):
+		super().__init__(groups)
+		self.frame_index = 0
+		self.animation_speed = 0.15
+		self.direction = pygame.math.Vector2()
 
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
+	def move(self,speed):
+		if self.direction.magnitude() != 0:
+			self.direction = self.direction.normalize()
 
-        self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
-        self.rect.center = self.hitbox.center
+		self.hitbox.x += self.direction.x * speed       #更新横坐标并判断水平碰撞
+		self.collision('horizontal')
+		self.hitbox.y += self.direction.y * speed		#更新纵坐标并判断垂直碰撞
+		self.collision('vertical')
+		self.rect.center = self.hitbox.center           #获得此时坐标
 
-    def collision(self, direction):  # 碰撞
-        if direction == 'horizontal':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0:
-                        self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x < 0:
-                        self.hitbox.left = sprite.hitbox.right
+	def collision(self,direction):
+		if direction == 'horizontal':
+			for sprite in self.obstacle_sprites:
+				if sprite.hitbox.colliderect(self.hitbox):
+					if self.direction.x > 0: # 右走代表另一个物体左侧收到碰撞
+						self.hitbox.right = sprite.hitbox.left
+					if self.direction.x < 0: # 左走代表另一个物体右侧收到碰撞
+						self.hitbox.left = sprite.hitbox.right
 
-        if direction == 'vertical':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0:
-                        self.hitbox.bottom = sprite.hitbox.top
-                    if self.direction.y < 0:
-                        self.hitbox.top = sprite.hitbox.bottom
+		if direction == 'vertical':
+			for sprite in self.obstacle_sprites:
+				if sprite.hitbox.colliderect(self.hitbox):
+					if self.direction.y > 0: # 下走上撞
+						self.hitbox.bottom = sprite.hitbox.top
+					if self.direction.y < 0: # 上走下撞
+						self.hitbox.top = sprite.hitbox.bottom
 
-    def wave_value(self):
-        value = sin(pygame.time.get_ticks())
-        if value >= 0:
-            return 255
-        else:
-            return 0
+	def wave_value(self):
+		value = sin(pygame.time.get_ticks())   #为了实现受到攻击后的虚化效果
+		if value >= 0: 
+			return 255
+		else: 
+			return 0
+
+	def getPos(self):
+		return (self.hitbox.x,self.hitbox.y)
